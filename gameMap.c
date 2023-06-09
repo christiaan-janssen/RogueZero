05c6cad0557e84c3e1be9188966e889127b50c6f
Christiaan Janssen <christiaanjanssen@outlook.com>
2023-06-09 06:59:01 +0200

    Add walls

--- gameMap.c
+++ gameMap.c
@@ -5,15 +5,19 @@
 GameMap *createMap(int width, int height, float tileSize) {
   GameMap *gameMap = malloc(sizeof(*gameMap) + width * height * sizeof(Tile));
 
-  Rectangle rect = {0, 0, tileSize, tileSize};
+  Rectangle floor_rect = {0, 0, tileSize, tileSize};
+  Rectangle wall_rect = {10 * tileSize, 17 * tileSize, tileSize, tileSize};
 
   for (size_t y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
       Vector2 pos = {x, y};
-      gameMap->tiles[coordToId(x, y, width)] =
-          createTile(pos, rect, false, false);
-      TraceLog(LOG_INFO,
-               TextFormat("ID: %i for {%i, %i}", coordToId(x, y, width), x, y));
+      if (x == 0 || x == width - 1 || y == 0 || y == height - 1) {
+        gameMap->tiles[coordToId(x, y, width)] =
+            createTile(pos, wall_rect, true, true);
+      } else {
+        gameMap->tiles[coordToId(x, y, width)] =
+            createTile(pos, floor_rect, false, false);
+      }
     }
   }
 
